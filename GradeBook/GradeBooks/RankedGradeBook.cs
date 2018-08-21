3846dc3152e03ad9f12a6dec5696efeaaf248c28
JeyLists <lisabeth.jen@hotmail.com>
2018-08-21 11:35:35 +0200

    Task 2

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
@@ -9,6 +11,24 @@ namespace GradeBook.GradeBooks
         RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            InvalidOperationException.Equals(Students.Count < 5, true);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).ToList();
+            int threshold = 0;
+
+            for (double i = 0.2, j = 0; i < 1.0; i += 0.2, j++)
+            {
+                threshold = (int)Math.Ceiling(Students.Count / i);
+                if (averageGrade >= grades[threshold].AverageGrade)
+                {
+                    return (char)('A' + j);
+                }
+            }
+
+            return 'F';
         }
     }
 }

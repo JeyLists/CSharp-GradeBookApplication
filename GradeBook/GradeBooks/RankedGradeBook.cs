ce5a1125a29da3df04923c6bbfe784ab9859ca4c
JeyLists <lisabeth.jen@hotmail.com>
2018-08-21 12:49:38 +0200

    Task 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -3,9 +3,9 @@ using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
-    class RankedGradeBook : BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
-        RankedGradeBook(string name) : base(name)
+        public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
         }
@@ -17,34 +17,16 @@ namespace GradeBook.GradeBooks
                 throw new InvalidOperationException("Cannot get letterGrade when there are less than 5 students");
             }
 
-            //var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
-            //int threshold = (int)Math.Ceiling(Students.Count * 0.2);
-            //
-            //if (grades[threshold - 1] <= averageGrade)
-            //{
-            //    return 'A';
-            //}
-            //else if (grades[(threshold * 2) - 1] <= averageGrade)
-            //{
-            //    return 'B';
-            //}
-            //else if (grades[(threshold * 3) - 1] <= averageGrade)
-            //{
-            //    return 'C';
-            //}
-            //else if (grades[(threshold * 4) - 1] <= averageGrade)
-            //{
-            //    return 'D';
-            //}
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+            int threshold = (int)Math.Ceiling(Students.Count * 0.2);
 
-            //for (int i = 0; i < 4; i++)
-            //{
-            //    if (grades[(threshold * (i+1)) - 1] <= averageGrade)
-            //    {
-            //        Console.WriteLine('A' + i);
-            //        return (char)('A' + i);
-            //    }
-            //}
+            for (int i = 0; i < 4; i++)
+            {
+                if (grades[(threshold * (i+1)) - 1] <= averageGrade)
+                {
+                    return (char)('A' + i);
+                }
+            }
 
             return 'F';
         }

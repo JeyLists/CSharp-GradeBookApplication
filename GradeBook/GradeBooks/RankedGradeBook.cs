e47c47f90f1624bbecd3bf5bd777be0072a43c2a
JeyLists <lisabeth.jen@hotmail.com>
2018-08-21 11:50:18 +0200

    Fix Task 2

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -19,13 +19,13 @@ namespace GradeBook.GradeBooks
             {
                 throw new InvalidOperationException("Cannot get letterGrade when there are less than 5 students");
             }
-            var grades = Students.OrderByDescending(e => e.AverageGrade).ToList();
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
             int threshold = 0;
 
             for (double i = 0.2, j = 0; i < 1.0; i += 0.2, j++)
             {
-                threshold = (int)Math.Ceiling(Students.Count / i);
-                if (averageGrade >= grades[threshold].AverageGrade)
+                threshold = (int)Math.Ceiling(Students.Count * i);
+                if (grades[threshold - 1] <= averageGrade)
                 {
                     return (char)('A' + j);
                 }

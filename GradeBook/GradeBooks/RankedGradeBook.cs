323462961ed02aba4f82c1dd9f0c59dcadf0b6ac
JeyLists <lisabeth.jen@hotmail.com>
2018-08-21 12:05:35 +0200

    Task 1.1

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -17,25 +17,25 @@ namespace GradeBook.GradeBooks
                 throw new InvalidOperationException("Cannot get letterGrade when there are less than 5 students");
             }
 
-            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
-            int threshold = (int)Math.Ceiling(Students.Count * 0.2);
-
-            if (grades[threshold - 1] <= averageGrade)
-            {
-                return 'A';
-            }
-            else if (grades[(threshold * 2) - 1] <= averageGrade)
-            {
-                return 'B';
-            }
-            else if (grades[(threshold * 3) - 1] <= averageGrade)
-            {
-                return 'C';
-            }
-            else if (grades[(threshold * 4) - 1] <= averageGrade)
-            {
-                return 'D';
-            }
+            //var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+            //int threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            //
+            //if (grades[threshold - 1] <= averageGrade)
+            //{
+            //    return 'A';
+            //}
+            //else if (grades[(threshold * 2) - 1] <= averageGrade)
+            //{
+            //    return 'B';
+            //}
+            //else if (grades[(threshold * 3) - 1] <= averageGrade)
+            //{
+            //    return 'C';
+            //}
+            //else if (grades[(threshold * 4) - 1] <= averageGrade)
+            //{
+            //    return 'D';
+            //}
 
             //for (int i = 0; i < 4; i++)
             //{
